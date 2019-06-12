d668187df663d333ca0f5647a8aa52f9fde1cb2d
Roselina Pradjanata <roselinapradjanata@gmail.com>
2019-06-12 12:22:01 +0700

    Refactor prices render function

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Empty } from 'antd';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -11,26 +12,30 @@ class Prices extends React.Component {
     const { type, data } = this.props;
     return (
       <React.Fragment>
-        <Title>{ type } Prices</Title>
-        <Table size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell>Date</TableCell>
-              <TableCell>Price</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data && data.map((row, i) => (
-              <TableRow key={i}>
-                <TableCell>{row.date}</TableCell>
-                <TableCell>{row.price}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+        <Title>{type} Prices</Title>
+        {data ? this.renderTable(data) : <Empty/>}
       </React.Fragment>
     );
   }
+
+  renderTable = (data) => (
+    <Table size="small">
+      <TableHead>
+        <TableRow>
+          <TableCell>Date</TableCell>
+          <TableCell>Price</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {data && data.map((row, i) => (
+          <TableRow key={i}>
+            <TableCell>{row.date}</TableCell>
+            <TableCell>{row.price}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
 
 export default Prices;
